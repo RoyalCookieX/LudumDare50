6cb6a651d550ff7e613cf12ae5b72bf025a79252
Karan Dhaliwal <Karandhal5050@gmail.com>
2022-04-01 23:57:06 -0700

    CoolDown Implimented

---
+++ Assets/LudumDare50/Scripts/CoolDown.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CoolDown : MonoBehaviour
+{
+    [SerializeField] Image CoolDownImage;
+    [SerializeField] TMP_Text number;
+
+    public void Cooldown(float WaitTime)
+    {
+        StartCoroutine(WaitDown(WaitTime));
+    }
+    private IEnumerator WaitDown(float WaitTime)
+    {
+        CoolDownImage.gameObject.SetActive(true);
+        CoolDownImage.fillAmount = 1;
+        number.text = WaitTime.ToString();
+        float WaitAmount = WaitTime;
+        while (0 < WaitTime)
+        {
+            yield return new WaitForSeconds(0.1f);
+            WaitTime -= 0.1f;
+            CoolDownImage.fillAmount = WaitTime / WaitAmount;
+            number.text = WaitTime.ToString("F1");
+        }
+        CoolDownImage.gameObject.SetActive(false);
+    }
+}
