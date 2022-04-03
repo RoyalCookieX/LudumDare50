7e4e927d1a368646d9d6b024f05f3859fc3c9657
Andrei Cabungcal <40614905+RoyalCookieX@users.noreply.github.com>
2022-04-03 01:51:03 -0500

    Fixed Player Movement

--- Assets/LudumDare50/Scripts/Player/PlayerMovement.cs
+++ Assets/LudumDare50/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -6,6 +7,16 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Tilemap _tilemap;
     [SerializeField] private int _gridZ;
     [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _minDistance;
+
+    private Vector3 _targetPosition;
+
+    private void Update()
+    {
+        if(((Vector2)_targetPosition - _rigidbody.position).sqrMagnitude > _minDistance * _minDistance)
+            _rigidbody.position = Vector2.MoveTowards(transform.position, _targetPosition, _speed * Time.deltaTime);
+    }
 
     public void Move(Vector2 worldPosition)
     {
@@ -13,7 +24,7 @@ public class PlayerMovement : MonoBehaviour
         gridPosition.z = _gridZ;
         if (_tilemap.HasTile(gridPosition))
         {
-            _rigidbody.position = worldPosition;
+            _targetPosition = worldPosition;
         }
     }
 }
